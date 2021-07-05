0257b4325fbe950c9bd94b42b2bb41e4f49281c3
prabhask5 <prabhas.kurapati@gmail.com>
2021-07-05 04:51:54 -0400

    connected ajax to sign up form + added input error for sign up and donation forms

--- website helpingsoup/forms.js
+++ website helpingsoup/forms.js
@@ -46,6 +46,7 @@ $(document).ready(function () {
       phone: $("#phone").val(),
       address: $("#address").val(),
       city: $("#city").val(),
+      state: $("#state").val(),
       zip: $("#zip").val(),
       date: $("#date").val(),
       time: $("#appt").val(),
@@ -78,7 +79,7 @@ $(document).ready(function () {
         if(e.target.id === "zip" && (e.target.value.length > 0 && (e.target.value.match(/^[0-9]+$/) == null || e.target.value.length !== 5))){
             setInputError(inputElement, "Please make sure you inputted your zip code in the correct format.");
         }
-        var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        var re = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
         if(e.target.id === "email" && (e.target.value.length > 0 && !re.test(e.target.value))){
             setInputError(inputElement, "Please make you inputted your email address in the corrent format.");
         }
--- website helpingsoup/login.js
+++ website helpingsoup/login.js
@@ -1,11 +1,3 @@
-function setFormMessage(formElement, type, message) {
-    const messageElement = formElement.querySelector(".form__message"); // looks at every html element with class form__message
-
-    messageElement.textContent = message; // changing noti to message passed in input
-    messageElement.classList.remove("form__message--success", "form__message--error"); //refeshing classes
-    messageElement.classList.add(`form__message--${type}`); // adding success or error class depending on input
-}
-
 function setInputError(inputElement, message) {
     inputElement.classList.add("form__input--error"); // change to error text
     inputElement.parentElement.querySelector(".form__input-error-message").textContent = message; // changes error to input message
@@ -19,6 +11,7 @@ function clearInputError(inputElement) {
 document.addEventListener("DOMContentLoaded", () => { // form has loaded
     const loginForm = document.querySelector("#login"); 
     const createAccountForm = document.querySelector("#createAccount");
+    var status = document.getElementById("status");
 
     document.querySelector("#linkCreateAccount").addEventListener("click", e => {
         e.preventDefault(); // preventing href link, or refreshing the page when link clicked
@@ -48,6 +41,7 @@ document.addEventListener("DOMContentLoaded", () => { // form has loaded
             email: $("#email").val(),
             address: $("#address").val(),
             city: $("#city").val(),
+            state: $("#state").val(),
             zip: $("#zip").val(),
             school: $("#school").val(),
             password: $("#password").val(),
@@ -62,10 +56,12 @@ document.addEventListener("DOMContentLoaded", () => { // form has loaded
           }).done(function (data) {
             console.log("success!");
             $(createAccountForm).trigger("reset");
-            setFormMessage(createAccountForm, "success", "Great! Your account has been made!");
+            $(status).addClass('success');
+            $(status).html("Great! Your account has been made!");
           }).fail(function (data) {
             console.log("error");
-            setFormMessage(createAccountForm, "error", "Sorry, an error occurred.");
+            $(status).addClass('error');
+            $(status).html("Sorry, an error has occurred");
           });
     })
 
@@ -78,7 +74,7 @@ document.addEventListener("DOMContentLoaded", () => { // form has loaded
             if(e.target.id === "zip" && (e.target.value.length > 0 && (e.target.value.match(/^[0-9]+$/) == null || e.target.value.length != 5))){
                 setInputError(inputElement, "Please make sure you inputted your zip code in the correct format.");
             }
-            var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            var re = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
             if(e.target.id === "email" && (e.target.value.length > 0 && !re.test(e.target.value))){
                 setInputError(inputElement, "Please make you inputted your email address in the corrent format.");
             }
