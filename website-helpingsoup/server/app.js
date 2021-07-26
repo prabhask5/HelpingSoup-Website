4f7e80fcb9efaf0aa705b2c6107e1b477171b734
prabhask5 <prabhas.kurapati@gmail.com>
2021-07-26 01:50:06 -0400

    organization + nodejs starting

---
+++ website-helpingsoup/server/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
+//create
+app.post('/insert', (request, reponse) =>{
+
+});
+
+//read
+app.get('/getAll', (request, response) => {
+    response.json({success:true});
+});
+
+//delete
+
+app.listen(process.env.PORT, () => console.log('app is running'));
