2c20e46e83b79bcb7bba5e03ce2058808adf733f
prabhask5 <prabhas.kurapati@gmail.com>
2021-07-25 22:17:34 -0400

    made id keys increment

--- docs/helpingSoupDB.sql
+++ docs/helpingSoupDB.sql
@@ -1,5 +1,5 @@
 create table customer(
-	customerID char(10) NOT NULL PRIMARY KEY,
+	customerID int IDENTITY(1, 1) NOT NULL PRIMARY KEY,
 	customerFirstName varchar(50) NULL,
 	customerLastName varchar(50) NULL,
 	customerPhone char(10) NULL,
@@ -10,8 +10,8 @@ create table customer(
 );
 
 create table goods(
-	goodsID char(10) NOT NULL PRIMARY KEY,
-	customerID char(10) FOREIGN KEY REFERENCES customer(customerID),
+	goodsID int IDENTITY(1, 1) NOT NULL PRIMARY KEY,
+	customerID int FOREIGN KEY REFERENCES customer(customerID),
 	pickupDate Date NULL,
 	pickupTime Time NULL,
 	startTime Datetime2 NULL,
@@ -25,16 +25,16 @@ create table volunteer(
 	volunteerStreetAddress varchar(100) NULL,
 	volunteerCity varchar(100) NULL,
 	volunteerZip char(5) NULL,
-	volunteerPassword varchar(100) NULL,
+	volunteerPassword varchar(100) NOT NULL,
 	volunteerSchool varchar(100) NULL,
 	volunteerFirstName varchar(50) NULL,
 	volunteerLastName varchar(50) NULL
 );
 
 create table volunteerDelivery(
-	deliveryNotesID char(10) NOT NULL PRIMARY KEY,
+	deliveryNotesID int IDENTITY(1, 1) NOT NULL PRIMARY KEY,
 	deliveryNotes varchar(250) NULL,
 	deliveryStatus Bit NULL,
 	volunteerEmail varchar(100) FOREIGN KEY REFERENCES volunteer(volunteerEmail),
-	goodsID char(10) FOREIGN KEY REFERENCES goods(goodsID)
+	goodsID int FOREIGN KEY REFERENCES goods(goodsID)
 );
