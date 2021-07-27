0056cb570b51ebc1296dab95624b06cc684799c5
prabhask5 <prabhas.kurapati@gmail.com>
2021-07-27 13:37:44 -0400

    working on db server

--- website-helpingsoup/server/app.js
+++ website-helpingsoup/server/app.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
-
+const server = require('./server')
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
