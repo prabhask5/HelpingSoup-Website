00c8d7f6cb4976452eaf5300e3a8418544e2269f
prabhask5 <prabhas.kurapati@gmail.com>
2021-08-07 12:51:13 -0400

    form fix

--- webdev/forms.js
+++ webdev/forms.js
@@ -24,8 +24,10 @@ function finalSubmit(canSubmit){
 document.addEventListener("DOMContentLoaded", () => {
   var form = document.getElementById("my-form");
   var status = document.getElementById("status");
-  var errors = 0;
+  var emailError = false;
+  var zipError = false;
   var timeError = false;
+  var rangeError = false;
   var canSubmit = true;
 
   $(document).on("click", "#donationSubmit", e => {
@@ -83,18 +85,18 @@ document.addEventListener("DOMContentLoaded", () => {
         if(e.target.id === "zip" && (e.target.value.length > 0 && (e.target.value.match(/^[0-9]+$/) == null || e.target.value.length !== 5))){
             setInputError(inputElement, "Please make sure you inputted your zip code in the correct format.");
             canSubmit = false;
-            errors++;
+            zipError = true;
         }
         var re = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
         if(e.target.id === "email" && (e.target.value.length > 0 && !re.test(e.target.value))){
             setInputError(inputElement, "Please make you inputted your email address in the correct format.");
             canSubmit = false;
-            errors++;
+            emailError = true;
         }
         if((e.target.id === "secondTime" || e.target.id === "firstTime") && (e.target.value < "10:00" || e.target.value > "20:00")){
           setInputError(inputElement, "Please make sure this time is within our office hours.");
           canSubmit = false;
-          errors++;
+          rangeError = true;
         }
         if(e.target.id === "secondTime" || e.target.id === "firstTime"){
           //console.log($("#1appt").val());
@@ -112,32 +114,34 @@ document.addEventListener("DOMContentLoaded", () => {
               setInputError(inputElement, "Please make sure your time range is 30 minutes or greater.");
               //setInputError($("#2appt"), "Please make sure your time range is 30 minutes or greater.");
               canSubmit = false;
-              errors++;
               timeError = true;
             }
           } 
         }
-        //console.log(errors + " " + canSubmit);
+       //console.log(errors + " " + canSubmit);
     });
 
     inputElement.addEventListener("input", e => { // when user types again
-        if(inputElement.id == "firstTime" || inputElement.id == "secondTime"){
+      if(inputElement.id == "firstTime"){
+        clearInputError(inputElement);
+        rangeError = false;
+      }  
+      else if(inputElement.id == "firstTime" || inputElement.id == "secondTime"){
           var t1 = document.querySelector("#firstTime");
           var t2 = document.querySelector("#secondTime");
           clearInputError(t1);
           clearInputError(t2);
           timeError = false;
-          if(errors == 0) errors = 0;
-          else errors--;
         }
-        else{
+        else if(inputElement.id == "zip"){
           clearInputError(inputElement);
-          if(errors == 0) errors = 0;
-          else errors--;
+          zipError = false;
         }
-        if(errors == 0){
-          canSubmit = true;
+        else if(inputElement.id == "email"){
+          clearInputError(inputElement);
+          emailError = false;
         }
+        if((!rangeError && !timeError) && (!zipError && !emailError)) canSubmit = true;
         //console.log(errors + " " + canSubmit);
     });
   });
--- webdev/login.js
+++ webdev/login.js
@@ -37,7 +37,9 @@ document.addEventListener("DOMContentLoaded", () => { // form has loaded
     const loginForm = document.querySelector("#login"); 
     const createAccountForm = document.querySelector("#createAccount");
     var status = document.getElementById("status");
-    var errors = 0;
+    var zipError = false;
+    var emailError = false;
+    var passError = false;
     var canSubmit = true;
 
     $(document).on("click", "#linkCreateAccount", e => {
@@ -156,26 +158,37 @@ document.addEventListener("DOMContentLoaded", () => { // form has loaded
             if (e.target.id === "confirmPassword" && (e.target.value.length > 0 && e.target.value !== $("#password").val())) {
                 setInputError(inputElement, "Please make sure your passwords match."); // calls setInputError method
                 canSubmit = false;
-                errors++;
+                passError = true;
             }
             if(e.target.id === "zip" && (e.target.value.length > 0 && (e.target.value.match(/^[0-9]+$/) == null || e.target.value.length != 5))){
                 setInputError(inputElement, "Please make sure you inputted your zip code in the correct format.");
                 canSubmit = false;
-                errors++;
+                zipError = true;
             }
             var re = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
             if(e.target.id === "email" && (e.target.value.length > 0 && !re.test(e.target.value))){
                 setInputError(inputElement, "Please make you inputted your email address in the correct format.");
                 canSubmit = false;
-                errors++;
+                emailError = true;
             }
+            //console.log("errors " + errors + " canSubmit " + canSubmit);;
         });
 
         inputElement.addEventListener("input", e => { // when user types again
-            clearInputError(inputElement); // clears error
-            if (errors == 0) errors = 0;
-            else errors--;
-            if (errors = 0) canSubmit = true;
+            if(inputElement.id == "password" || inputElement.id == "confirmPassword"){
+                clearInputError(inputElement);
+                passError = false;
+            }
+            else if(inputElement.id == "zip"){
+                clearInputError(inputElement);
+                zipError = false;
+            }
+            else if(inputElement.id == "email" || inputElement.id == "loginEmail"){
+                clearInputError(inputElement);
+                emailError = false;
+            }
+                if((!rangeError && !timeError) && (!zipError && !emailError)) canSubmit = true;
+            //console.log("errors " + errors + " canSubmit " + canSubmit);
         });
     });
 });
