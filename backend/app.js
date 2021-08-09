9fde46f03d0929365ddd02c54229f40d6551a93e
mihirkonda <mihir.konda@gmail.com>
2021-08-09 14:46:48 -0400

    did madness and got everything

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
-const mailer = require('nodemailer');
 dotenv.config();
 
 const DbService = require('./dBConnection');
@@ -11,15 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-const transport = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    //port: process.env.EMAIL_PORT,
-    secure: true,
-    auth: {
-       user: process.env.EMAIL_USER,
-       pass: process.env.EMAIL_PASS
-    }
-});
 
 app.post('/volunteerSignUp', (request, response) =>{
     const formData = request.body;
