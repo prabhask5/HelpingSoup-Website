c80bc20861b3cad5abd3a38562b829a7320bb96d
prabhask5 <prabhas.kurapati@gmail.com>
2021-08-15 17:44:57 -0400

    Revert "Merge branch 'prabhasDev' into main"
    This reverts commit 4b52bea83a566cacbbac4179a14143b687f70a85, reversing
    changes made to 6e118acd4005276cffffc51f822c0b4564fe9a29.

--- backend/app.js
+++ backend/app.js
@@ -2,90 +2,41 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
-const crypto = require('crypto');
-const nodemailer = require('nodemailer');
+const mailer = require('nodemailer');
 dotenv.config();
+
 const DbService = require('./dBConnection');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-var transporter = nodemailer.createTransport({
-    service: process.env.EMAIL_HOST,
+const transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    //port: process.env.EMAIL_PORT,
+    secure: true,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
+       user: process.env.EMAIL_USER,
+       pass: process.env.EMAIL_PASS
     }
-  });
+});
 
 app.post('/volunteerSignUp', (request, response) =>{
     const formData = request.body;
     const db = DbService.getDbServiceInstance();
-    const result1 = db.getLogin(formData.email)
-    result1
-    .then(data => {
-        if(data.length > 0){
-            response.json({success: false});
-        }
-        else{
-            const result2 = db.insertVolunteer(formData.firstName, formData.lastName, formData.email,
-                formData.address, formData.city, formData.state,
-                 formData.zip, formData.school, formData.password);
-           result2
-           .then(response.json({success: true}));
-        }
-    })
+    const result = db.insertVolunteer(formData.firstName, formData.lastName, formData.email,
+         formData.address, formData.city, formData.state,
+          formData.zip, formData.school, formData.password);
+    result
+    .then(data => response.json({data: data}));
 });
 
 app.post('/volunteerLogin', (request, response) => {
     const formData = request.body;
     const db = DbService.getDbServiceInstance();
-    const result = db.getLogin(formData.email);
+    const result = db.getLogin(formData.email, formData.password);
     result
-    .then(data => {
-        pass = []
-        for(var i = 0; i < data.length; i++){
-            pass.push(data[i].volunteerPassword);
-        }
-        var canLogin = false;
-        for(var i = 0; i < pass.length; i++){
-            if(pass[i] == formData.password) canLogin = true;
-        }
-        response.json({CanLogin: canLogin});
-    });
-});
-
-app.post('/forgotPasswordEmail', (request, response) => {
-    const formData = request.body;
-    const db = DbService.getDbServiceInstance();
-    const result1 = db.findEmail(formData.email);
-    result1
-    .then(data => {
-        if(data.length > 0){
-            db.updateToken(formData.email);
-            var token = crypto.randomBytes(64).toString('base64');
-            var expireDate = new Date();
-            expireDate.setHours(expireDate.getHours() + 1);
-            db.createToken(formData.email, expireDate, token);
-            const message = {
-                from: process.env.EMAIL_USER,
-                to: formData.email,
-                subject: "HelpingSoup Reset Password",
-                text: 'To reset your password, please click the link below.\n\nhttp://' + process.env.DOMAIN + '/user/reset-password?token='+ encodeURIComponent(token) +'&email='+ formData.email
-            };
-            transporter.sendMail(message, (err, info) => {
-                if(err) console.log(err);
-                //else console.log(info);
-            });
-            response.json({success: true});
-        }
-        else response.json({success: false});
-    });
-});
-
-app.get('/user/reset-password', (request, response, next) => {
-    response.sendFile('login.html');
+    .then(data => response.json({data: data}));
 });
 
 app.post('/donation', (request, response) => {
--- frontend/login.js
+++ frontend/login.js
@@ -8,21 +8,22 @@ function clearInputError(inputElement) {
     inputElement.parentElement.querySelector(".form__input-error-message").textContent = ""; // removes message
 }
 
-function finalSubmit(canSubmit, mode){
+function finalSignUpSubmit(canSubmit){
     var ret = true;
-    var selectClass = "";
-    switch(mode){
-        case 0:
-            selectClass = ".form__input";
-            break;
-        case 1:
-            selectClass = ".loginform__input";
-            break;
-        case 2:
-            selectClass = ".forgotform__input";
-            break;
-    }
-    document.querySelectorAll(selectClass).forEach(inputElement => {
+    document.querySelectorAll(".form__input").forEach(inputElement => {
+        if(inputElement.value.length === 0){
+            setInputError(inputElement, "Please enter information here.");
+            ret = false;
+        }
+    });
+    //console.log("Ret is " + ret);
+    if(canSubmit == false) ret = false;
+    return ret;
+}
+
+function finalLoginSubmit(canSubmit){
+    var ret = true;
+    document.querySelectorAll(".loginform__input").forEach(inputElement => {
         if(inputElement.value.length === 0){
             setInputError(inputElement, "Please enter information here.");
             ret = false;
@@ -37,12 +38,10 @@ document.addEventListener("DOMContentLoaded", () => { // form has loaded
     const createAccountForm = document.querySelector("#createAccount");
     const forgotForm = document.querySelector("#forgot");
     var status = document.getElementById("status");
-    var canSubmitSignUp = true;
-    var canSubmitForgot = true;
-    var signUpPassError = false;
-    var forgotPassError = false;
-    var emailError = false;
     var zipError = false;
+    var emailError = false;
+    var passError = false;
+    var canSubmit = true;
 
     $(document).on("click", "#linkCreateAccount", e => {
         e.preventDefault(); // preventing href link, or refreshing the page when link clicked
@@ -69,8 +68,8 @@ document.addEventListener("DOMContentLoaded", () => { // form has loaded
         e.preventDefault();
         $(status).removeClass();
         $(status).html("");
-        var submit = finalSubmit(true, 1);
-        if(submit){
+        canSubmit = finalLoginSubmit(canSubmit);
+        if(canSubmit){
             var formData = {
                 email: $("#loginEmail").val(),
                 password: $("#loginPass").val(),
@@ -79,80 +78,53 @@ document.addEventListener("DOMContentLoaded", () => { // form has loaded
                 type: "POST",
                 url: "http://localhost:4000/volunteerLogin",
                 data: formData,
-                dataType: "json",
+                dataType: "text",
                 encode: true,
                 success: function(data){
-                    if(data.CanLogin == true){
-                        console.log("logged in!");
+                    var obj = JSON.parse(data);
+                    pass = []
+                    for(var i = 0; i < obj.data.length; i++){
+                        pass.push(obj.data[i].volunteerPassword);
+                    }
+                    console.log("success!");
+                    var canLogin = false;
+                    for(var i = 0; i < pass.length; i++){
+                        if(pass[i] == formData.password) canLogin = true;
+                    }
+                    if(canLogin){
                         $(loginForm).trigger("reset");
                         $(status).addClass('success');
                         $(status).html("Great! You've been logged in (theoretically)!");
                     }
                     else{
-                        console.log("didn't work!");
-                        alert("Sorry, this username and password combination is incorrect. Please try again.");
+                        $(loginForm).trigger("reset");
+                        $(status).addClass('error');
+                        $(status).html("Sorry, this username and password combination is incorrect. Please try again.");
                     }
                 },
                 error: function(data){
                     console.log("error");
                     $(loginForm).trigger("reset");
-                    alert("Sorry, an error has occurred. Please try again later.");
+                    $(status).addClass('error');
+                    $(status).html("Sorry, an error has occurred. Please try again later.");
                 }
               });
         }
         else{
             console.log("Can't  submit!");
-            alert("Sorry, an error occurred with your inputs. Please try again.");
+            $(status).addClass('error');
+            $(status).html("Sorry, an error occurred with your inputs. Please try again.");
         }
-    });
 
-    $(document).on("click", "#forgotSubmit", e => {
-        e.preventDefault();
-        $(status).removeClass();
-        $(status).html("");
-        var submit = finalSubmit(canSubmitForgot, 2);
-        if(submit){
-            var formData = {
-                email: $("#forgotEmail").val(),
-            };
-            $.ajax({
-                type: "POST",
-                url: "http://localhost:4000/forgotPasswordEmail",
-                data: formData,
-                dataType: "json",
-                encode: true,
-                success: function(data){
-                    if(data.success == true){
-                        console.log("success!");
-                        $(forgotForm).trigger("reset");
-                        $(status).addClass('success');
-                        $(status).html("Great! An email has been sent to this address.");
-                    }
-                    else{
-                        console.log("didn't work!");
-                        $(forgotForm).trigger("reset");
-                        alert("Sorry, this email does not exist within our records. Please try again.");
-                    }
-                },
-                error: function(data){
-                    console.log("error");
-                    $(loginForm).trigger("reset");
-                    alert("Sorry, an error has occurred. Please try again later.");
-                }
-              });
-        }
-        else{
-            console.log("Can't  submit!");
-            alert("Sorry, an error occurred with your inputs. Please try again.");
-        }
+        //setFormMessage(loginForm, "error", "Invalid email/password combination"); // default error message
     });
 
     $(document).on("click", "#signUpSubmit", e => {
         e.preventDefault();
         $(status).removeClass();
         $(status).html("");
-        var submit = finalSubmit(canSubmitSignUp, 0);
-        if(submit) {
+        canSubmit = finalSignUpSubmit(canSubmit);
+        if(canSubmit) {
             var formData = {
                 firstName: $("#firstName").val(),
                 lastName: $("#lastName").val(),
@@ -163,76 +135,69 @@ document.addEventListener("DOMContentLoaded", () => { // form has loaded
                 zip: $("#zip").val(),
                 school: $("#school").val(),
                 password: $("#password").val(),
-              };
+              }
             $.ajax({
                 type: "POST",
                 url: "http://localhost:4000/volunteerSignUp",
                 data: formData,
-                dataType: "json",
+                dataType: "text",
                 encode: true,
                 success: function(data){
-                    if(data.success == true){
-                        console.log("success!");
-                        $(createAccountForm).trigger("reset");
-                        $(status).addClass('success');
-                        $(status).html("Great! Your account has been made!");
-                    }
-                    else{
-                        console.log("email already in use");
-                        alert("Sorry, that email is already in use. Please try again.");
-                    }
+                    console.log("success!");
+                    $(createAccountForm).trigger("reset");
+                    $(status).addClass('success');
+                    $(status).html("Great! Your account has been made!");
                 },
                 error: function(data){
                     console.log("error");
                     $(createAccountForm).trigger("reset");
-                    alert("Sorry, an error has occurred. Please try again later.");
+                    $(status).addClass('error');
+                    $(status).html("Sorry, an error has occurred. Please try again later.");
                 }
               });
         }
         else {
             console.log("Can't submit!");
-            alert("Sorry, an error occurred with your inputs. Please try again.");
+            $(status).addClass('error');
+            $(status).html("Sorry, an error occurred with your inputs. Please try again.");
         }
     });
 
-    document.querySelectorAll(".form__input, .forgotform__input, .loginform__input").forEach(inputElement => { // looks at all form__input classes
+    document.querySelectorAll(".form__input, .loginform__input").forEach(inputElement => { // looks at all form__input classes
         inputElement.addEventListener("blur", e => { // when user inputs something then clicks off
-            if ((e.target.id === "confirmPassword" || e.target.id === "password") && (($("#confirmPassword").val().length > 0 && $("#password").val().length > 0) && $("#confirmPassword").val() !== $("#password").val())) {
+            if (e.target.id === "confirmPassword" && (e.target.value.length > 0 && e.target.value !== $("#password").val())) {
                 setInputError(inputElement, "Please make sure your passwords match."); // calls setInputError method
-                canSubmitSignUp = false;
-                signUpPassError = true;
+                canSubmit = false;
+                passError = true;
             }
             if(e.target.id === "zip" && (e.target.value.length > 0 && (e.target.value.match(/^[0-9]+$/) == null || e.target.value.length != 5))){
                 setInputError(inputElement, "Please make sure you inputted your zip code in the correct format.");
-                canSubmitSignUp = false;
+                canSubmit = false;
                 zipError = true;
             }
             var re = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
             if(e.target.id === "email" && (e.target.value.length > 0 && !re.test(e.target.value))){
                 setInputError(inputElement, "Please make you inputted your email address in the correct format.");
-                canSubmitSignUp = false;
+                canSubmit = false;
                 emailError = true;
             }
-            //console.log("errors " + errors + " canSubmit " + canSubmit);
+            //console.log("errors " + errors + " canSubmit " + canSubmit);;
         });
 
         inputElement.addEventListener("input", e => { // when user types again
             if(inputElement.id == "password" || inputElement.id == "confirmPassword"){
                 clearInputError(inputElement);
-                signUpPassError = false;
+                passError = false;
             }
             else if(inputElement.id == "zip"){
                 clearInputError(inputElement);
                 zipError = false;
             }
-            else if(inputElement.id == "email"){
+            else if(inputElement.id == "email" || inputElement.id == "loginEmail"){
                 clearInputError(inputElement);
                 emailError = false;
             }
-            else{
-                clearInputError(inputElement);
-            }
-            if((!signUpPassError) && (!zipError && !emailError)) canSubmitSignUp = true;
+                if((!passError) && (!zipError && !emailError)) canSubmit = true;
             //console.log("errors " + errors + " canSubmit " + canSubmit);
         });
     });
